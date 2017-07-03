8c19e37a4fe9a39ad5c272f7ff82f27e767511e3
LmoonnightZ <273447903@qq.com>
2017-07-03 17:45:10 +0800

    添加用户列表路由

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { HashRouter  as Router, Route} from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import UserAddPage from './pages/UserAdd';
 import HomePage from './pages/Home';
-import Usual from './components/Usual';
+import UserListPage from './pages/UserList';
 const hashHistory = createBrowserHistory();
 
 ReactDOM.render((
@@ -12,6 +12,7 @@ ReactDOM.render((
        <div>
            <Route path="/" exact  component={HomePage}/>
            <Route path="/user/add" component={UserAddPage}/>
+           <Route path="/user/list" component={UserListPage}/>
        </div>
     </Router>
 ), document.getElementById('app'));
