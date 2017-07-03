abdc4925c9c99e78ecf3b5a916f2f3c77006e6ec
LmoonnightZ <273447903@qq.com>
2017-07-03 17:24:12 +0800

    UserAdd.js中使用FormItem组件

--- src/pages/UserAdd.js
+++ src/pages/UserAdd.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormProvider from './../utils/FormProvider';
+import FormItem from './../utils/FormItem';
 class UserAdd extends React.Component {
 
     handleSubmit=(e)=>{
@@ -42,28 +43,32 @@ class UserAdd extends React.Component {
 
                 <main>
                     <form onSubmit={(e)=>{this.handleSubmit(e)}}>
-                        <label>用户名：</label>
-                        <input type="text"
-                               value={name.value}
-                               onChange={(e) => onFormChange('name', e.target.value)}/>
-                        {!name.valid && <span>{name.error}</span>}
-                        <br/>
-                        <label>年龄：</label>
-                        <input type="number"
-                               value={age.value|| ''}
-                               onChange={(e) => onFormChange('age', e.target.value, 'number')}/>
-                        {!age.valid && <span>{age.error}</span>}
-                        <br/>
-                        <label>性别：</label>
-                        <select value={gender.value}
-                                onChange={(e) => onFormChange('gender', e.target.value)} >
-                            <option value="">请选择</option>
-                            <option value="male">男</option>
-                            <option value="female">女</option>
-                        </select>
-                        {!gender.valid && <span>{gender.error}</span>}
-                        <br/>
-                        <br/>
+
+                        <FormItem label="用户名：" valid={name.valid} error={name.error}>
+                                <input
+                                    type="text"
+                                    value={name.value}
+                                    onChange={(e) => onFormChange('name', e.target.value)}
+                                />
+                        </FormItem>
+
+                        <FormItem label="年龄:" valid={age.valid} error={age.error}>
+                            <input
+                                type="number"
+                                value={age.value}
+                                onChange={(e) => onFormChange('age', e.target.value, 'number')}
+                            />
+                        </FormItem>
+
+                        <FormItem label="性别:" valid={gender.valid} error={gender.error}>
+                            <select value={gender.value}
+                                     onChange={(e) => onFormChange('gender', e.target.value)} >
+                                <option value="">请选择</option>
+                                <option value="male">男</option>
+                                <option value="female">女</option>
+                            </select>
+                        </FormItem>
+
                         <input type="submit" value="提交"/>
                     </form>
                 </main>
